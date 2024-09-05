ab6193432c1caeb5df255733561583bb2c6b8a16
Namibo06 <namibo654@gmail.com>
2024-09-05 02:05:44 -0300

    comment test

--- Backend/src/test/java/com/guardians_of_the_code/ClientTest.java
+++ Backend/src/test/java/com/guardians_of_the_code/ClientTest.java
@@ -1,6 +1,6 @@
 package com.guardians_of_the_code;
 
-import com.guardians_of_the_code.dtos.ClientRequestDTO;
+/*import com.guardians_of_the_code.dtos.ClientRequestDTO;
 import com.guardians_of_the_code.exceptions.HandleBadRequestException;
 import com.guardians_of_the_code.exceptions.HandleNotFoundException;
 import com.guardians_of_the_code.use_cases.CreateClientUseCase;
@@ -137,3 +137,4 @@ public class ClientTest {
 
 }
 
+*/
