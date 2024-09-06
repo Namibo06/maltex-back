f6f78fedaff35a3cbc6fba73a2a6ff82924ceda2
Namibo06 <namibo654@gmail.com>
2024-09-06 04:16:33 -0300

    refactor: swagger

--- Backend/src/main/java/com/guardians_of_the_code/configs/SwaggerConfig.java
+++ Backend/src/main/java/com/guardians_of_the_code/configs/SwaggerConfig.java
@@ -1,12 +1,12 @@
 package com.guardians_of_the_code.configs;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemeType;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
