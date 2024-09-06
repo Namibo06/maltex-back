863d91a97b0f3da253732714de0cff5899474a6f
Namibo06 <namibo654@gmail.com>
2024-09-06 16:01:29 -0300

    fix: cors

--- Backend/src/main/java/com/guardians_of_the_code/configs/CorsConfig.java
+++ Backend/src/main/java/com/guardians_of_the_code/configs/CorsConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
