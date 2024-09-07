3eb2eb0a73f899690c968e64592807eed0adca31
Namibo06 <namibo654@gmail.com>
2024-09-07 14:05:47 -0300

    fixx: cors

--- Backend/src/main/java/com/guardians_of_the_code/configs/CorsConfig.java
+++ Backend/src/main/java/com/guardians_of_the_code/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://malte-x.vercel.app/","http://localhost:5173/")); // Ajuste conforme necessário
+        configuration.setAllowedOrigins(List.of("https://malte-x.vercel.app/","http://localhost:5173/","http://localhost:5173")); // Ajuste conforme necessário
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Se necessário
